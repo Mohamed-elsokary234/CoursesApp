34b2d0ad891ce8a109a6085bf62c55bbc3d2312f
Mohamed el-sokary <mohamed.elsokary234@gmail.com>
2021-10-26 01:24:43 +0200

    first commit

---
+++ src/app/students/content/personal_data/personal_data.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-personal-data',
+  templateUrl: './personal_data.component.html',
+  styleUrls: ['./personal_data.component.css']
+})
+export class PersonalDataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
